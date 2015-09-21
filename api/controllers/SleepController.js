621e8ffbe916a6983dde3c42d03011b69b3592a3
Michael Marcinek <michaelmarcinek@gmail.com>
2015-09-20 22:02:04 -0400

    creates a post and get route

--- api/controllers/SleepController.js
+++ api/controllers/SleepController.js
@@ -21,6 +21,68 @@ module.exports = {
       res.json(sleep);
 
     });
+
+  },
+
+  // FIND by ID:
+  find: function (req, res, next) {
+
+    var id = req.param('id');
+
+    var idShortCut = isShortcut(id);
+
+    if (idShortCut === true) {
+      return next ();
+    }
+
+    if (id) {
+
+      Sleep.findOne(id, function (err, sleep) {
+
+        if(sleep === undefined) return res.notFound();
+
+        if (err) return next(err);
+
+        res.json(sleep);
+      });
+
+    } else {
+
+      var where = req.param('where');
+
+      if (_.isString(where)) {
+        where = JSON.parse(where);
+      }
+
+      var options = {
+          limit: req.param('limit') || undefined,
+          skip: req.param('skip') || undefined,
+          sort: req.param('sort') || undefined,
+          where: where || undefined
+      };
+
+      console.log("These are the options", options);
+
+    Sleep.find(options, function(err, sleep) {
+
+      if(sleep === undefined) return res.notFound();
+
+      if(err) return next(err);
+
+      res.json(sleep);
+
+    });
+
   }
+
+    function isShortcut(id) {
+      if (id === 'find' || id === 'update' || id === 'create' || id === 'destroy') {
+        return true;
+      }
+
+    }
+
+  }
+
 };
 
